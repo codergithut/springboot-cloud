f937dde2278fd8de4a34cd2f0e92af707a0c4e8e
tianjian <1468195034@qq.com>
2018-04-08 16:46:02 +0800

    测试

---
+++ data-elasticsearch/src/test/java/elasticsearch/RestTemplateTest.java
@@ -0,0 +1,41 @@
+package elasticsearch;
+
+import com.domain.entity.HotWorld;
+import com.netflix.ribbon.proxy.annotation.Http;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RestTemplateTest {
+
+    @Test
+    public void restTest() {
+        RestTemplate restTemplate=new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+//  请勿轻易改变此提交方式，大部分的情况下，提交方式都是表单提交
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+//  封装参数，千万不要替换为Map与HashMap，否则参数无法传递
+        MultiValueMap params= new LinkedMultiValueMap<String, String>();
+//  也支持中文
+        params.add("begin", "1");
+        params.add("end", "5");
+        params.add("size", "10");
+        params.add("caseType", "72");
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+        //HotWorld[] obs = restTemplate.getForObject("http://127.0.0.1:8081/api/hotWorld?begin=1&end=6&size=10&caseType=72", HotWorld[].class, param);
+        Object obs1 = restTemplate.postForObject("http://127.0.0.1:8081/api/hotWorld", requestEntity, HotWorld.class);
+        System.out.println("");
+//        System.out.println(obs.length);
+    }
+
+}
