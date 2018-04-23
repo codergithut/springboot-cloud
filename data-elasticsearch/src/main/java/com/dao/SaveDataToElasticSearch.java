4211d048fe0411ec333dba1bd53f8fe20751abe2
tianjian <1468195034@qq.com>
2018-04-23 13:30:44 +0800

    ElasticSearch 批量数据录入测试

--- data-elasticsearch/src/main/java/com/dao/SaveDataToElasticSearch.java
+++ data-elasticsearch/src/main/java/com/dao/SaveDataToElasticSearch.java
@@ -1,15 +1,26 @@
 package com.dao;
 
+import com.config.ConstantParam;
 import com.domain.ElasticEntityProxy;
 import com.domain.IndexParam;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class SaveDataToElasticSearch {
@@ -37,6 +48,45 @@ public class SaveDataToElasticSearch {
         request.source(elasticEntityProxy.toString(), XContentType.JSON);
         IndexResponse response =  client.index(request);
         System.out.println();
+    }
+
+    /**
+     * @param elasticEntityProxyList 需要插入的elasticsearch数据
+     * @throws IOException 数据流异常
+     */
+    public void bulkAddElasticSearch(List<ElasticEntityProxy> elasticEntityProxyList) throws IOException {
+
+        BulkRequest request = new BulkRequest();
+
+        for(ElasticEntityProxy elasticEntityProxy : elasticEntityProxyList) {
+            IndexParam indexParam = elasticEntityProxy.getIndex();
+            request.add(new IndexRequest(
+                    indexParam.getIndex(),
+                    indexParam.getType(),
+                    elasticEntityProxy.getProxyEntry().getMainId())
+                    .source(XContentType.JSON,elasticEntityProxy.toString()));
+        }
+        BulkResponse bulkResponse = client.bulk(request);
+
+        for (BulkItemResponse bulkItemResponse : bulkResponse) {
+            DocWriteResponse itemResponse = bulkItemResponse.getResponse();
+
+            if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.INDEX
+                    || bulkItemResponse.getOpType() == DocWriteRequest.OpType.CREATE) {
+                IndexResponse indexResponse = (IndexResponse) itemResponse;
+
+            } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.UPDATE) {
+                UpdateResponse updateResponse = (UpdateResponse) itemResponse;
+
+            } else if (bulkItemResponse.getOpType() == DocWriteRequest.OpType.DELETE) {
+                DeleteResponse deleteResponse = (DeleteResponse) itemResponse;
+            }
+
+            if (bulkItemResponse.isFailed()) {
+                BulkItemResponse.Failure failure = bulkItemResponse.getFailure();
+            }
+        }
+
     }
 
 
